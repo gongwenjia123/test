8ca2a0f47abfdff581633fd7c49bec92d9650a58
gongwenjia123 <gongwenjia@icloud.com>
2015-01-25 17:11:23 +0800

    Revert "Revert "Revert "添加启动提示"""
    This reverts commit bbcec8f9a322e6a3feeff085a95daef2891788ce.

--- conditiontest/ConditionTest/AppDelegate.m
+++ conditiontest/ConditionTest/AppDelegate.m
@@ -16,7 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    NSLog(@"启动");
+    // Override point for customization after application launch.
     return YES;
 }
 
